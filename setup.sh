b00ad216eb6f0ff4ec5f5c4b036fc1dd213612c7
rayprj <sploga.murugan@gmail.com>
2017-09-26 19:43:22 +0530

    added sudo apt-get install default-jre

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install jenkins -y
-
+sudo apt-get install default-jre -y
 sudo apt-get install apache2 -y
 sudo a2enmod proxy
 sudo a2enmod proxy_http
